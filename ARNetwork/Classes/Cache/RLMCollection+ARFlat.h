f52795aa6bbf0db4c40a7be550c19591458a502e
Daniel Lin <linzhdk@gmail.com>
2017-08-04 21:38:09 +0800

    Add flat func for RLMCollection

---
+++ ARNetwork/Classes/Cache/RLMCollection+ARFlat.h
@@ -0,0 +1,11 @@
+//
+//  RLMCollection+ARFlat.h
+//  Pods
+//
+//  Created by Daniel Lin on 04/08/2017.
+//
+//
+
+#import <Realm/Realm.h>
+
+
---
+++ ARNetwork/Classes/Cache/RLMCollection+ARFlat.m
@@ -0,0 +1,11 @@
+//
+//  RLMCollection+ARFlat.m
+//  Pods
+//
+//  Created by Daniel Lin on 04/08/2017.
+//
+//
+
+#import "RLMCollection+ARFlat.h"
+
+
